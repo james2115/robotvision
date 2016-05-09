e4f385f308a453f58f07cb7657955ebafaccd2f7
James Fisher <james@waters-fisher.id.au>
2016-05-09 02:44:25 +0000

    Tested hall effect

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,61 @@
+#if 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <pigpio.h>
+
+/*
+OH3144E or equivalent Hall effect sensor
+
+Pin 1 - 5V
+Pin 2 - Ground
+Pin 3 - gpio (here P1-8, gpio 14, TXD is used)
+
+The internal gpio pull-up is enabled so that the sensor
+normally reads high.  It reads low when a magnet is close.
+
+gcc -o hall hall.c -lpigpio -lrt -lpthread
+sudo ./hall
+*/
+
+#define HALL 14
+
+void alert(int gpio, int level, uint32_t tick)
+{
+   static uint32_t lastTick=0;
+
+   if (lastTick) printf("%d %.6f\n", level, (float)(tick-lastTick)/1000000.0);
+   else          printf("%d-0.000000\n", level);
+
+   lastTick = tick;
+}
+
+int main(int argc, char *argv[])
+{
+   int secs=60;
+
+   if (argc>1) secs = atoi(argv[1]); /* program run seconds */
+
+   if ((secs<1) || (secs>3600)) secs = 3600;
+
+   if (gpioInitialise()<0) return 1;
+
+   gpioSetMode(HALL, PI_INPUT);
+
+   gpioSetPullUpDown(HALL, PI_PUD_UP);
+//   gpioSetPullUpDown(HALL, PI_PUD_DOWN);
+
+   gpioSetAlertFunc(HALL, alert);
+
+   sleep(secs);
+
+   gpioTerminate();
+}
+
+#else
+
 #include <ctime>
 #include <iostream>
 #include <opencv2/highgui/highgui.hpp>
@@ -130,3 +188,5 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
+
+#endif
